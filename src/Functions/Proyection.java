c49bb7fa394dcbabd974ab34dd0a3871888328c4
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-09-30 09:43:11 +0100

    Proyection done

---
+++ src/Functions/Proyection.java
@@ -0,0 +1,30 @@
+package src.Functions;
+
+import src.NaturalNumber;
+
+public class Proyection implements PrimitiveRecursiveFunction  {
+  private Number inputNumber;
+  private Number outputNumber;
+  private Number objetiveIndex;
+  private NaturalNumber result;
+
+  public Proyection(Number inputNumber, Number outputNumber, Number objetiveIndex) {
+    this.inputNumber = inputNumber;
+    this.outputNumber = outputNumber;
+    this.objetiveIndex = objetiveIndex.intValue();
+    this.result = new NaturalNumber(-1);
+  }
+  public NaturalNumber operate(NaturalNumber n) {
+    // if (this.inputNumber.intValue() > this.result.getNumbers().size() || this.outputNumber.intValue() != 1) {
+    //   throw new IllegalArgumentException("Proyection function only accepts 1 as output value and the input value must be less than the number of elements in the input list");
+    // }
+    result = new NaturalNumber(n.getNumbers().get(objetiveIndex.intValue() - 1));
+    return result;
+  }
+  public Number inputValues() {
+    return inputNumber;
+  }
+  public Number outputValues() {
+    return outputNumber;
+  }
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 package src;
 
 import src.Functions.PrimitiveRecursiveFunction;
+import src.Functions.Proyection;
 import src.Functions.Sucesor;
 import src.Functions.Zero;
 
@@ -10,6 +11,9 @@ public class Main {
     PrimitiveRecursiveFunction f = new Zero(n.getNumbers().size(), 1);
     n = f.operate(n);
     f = new Sucesor(1, 1);
+    n.add(f.operate(n).getNumbers().get(0));
+    f = new Proyection(n.getNumbers().size(), 1, 2);
+    System.out.println(n);
     n = f.operate(n);
     System.out.println(n);
   }
