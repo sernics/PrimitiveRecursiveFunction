f6aee61b7259ca191d2d71aad6cea7b19b242314
Sergio Nicolas Segui <sergionicolassegui@mac.lan>
2024-10-01 22:07:41 +0100

    Product finished and tested

--- src/Functions/Add.java
+++ src/Functions/Add.java
@@ -2,12 +2,12 @@ package src.Functions;
 
 import src.NaturalNumber;
 
-public class Suma implements PrimitiveRecursiveFunction {
+public class Add implements PrimitiveRecursiveFunction {
   private NaturalNumber result;
   private Number inputValues;
   private Number outputValues;
   
-  public Suma() {
+  public Add() {
     this.inputValues = 2;
     this.outputValues = 1;
     this.result = new NaturalNumber(-1);
@@ -16,17 +16,16 @@ public class Suma implements PrimitiveRecursiveFunction {
     // The value that are going to be changing is the second value of the list, and when this value reach 0, 
     // the base case is going to be executed
     if (n.getNumbers().get(1).intValue() == 0) {
-      return new NaturalNumber(n.getNumbers().get(0));
+      PrimitiveRecursiveFunction proyection = new Proyection(2, 1, 1);
+      return proyection.operate(n);
     } else {
       PrimitiveRecursiveFunction sucesor = new Sucesor(1, 1);
       PrimitiveRecursiveFunction proyection = new Proyection(3, 1, 3);
-
+      
+      // (x, y, add(x, y)) -> parameters
       NaturalNumber parameters = new NaturalNumber(n.getNumbers().get(0).intValue());
       parameters.add(n.getNumbers().get(1).intValue() - 1);
-
-      NaturalNumber value = this.operate(parameters);
-
-      parameters.add(value.getNumbers().get(0).intValue());
+      parameters.add(this.operate(parameters).getNumbers().get(0).intValue());
       
       PrimitiveRecursiveFunction composition = new Composition(sucesor);
       this.result = composition.operate(proyection.operate(parameters));
--- src/main.java
+++ src/main.java
@@ -3,15 +3,15 @@ package src;
 import src.Functions.Combination;
 import src.Functions.Composition;
 import src.Functions.PrimitiveRecursiveFunction;
-import src.Functions.Suma;
+import src.Functions.Product;
 
 public class Main {
   public static void main(String[] args) {
-    NaturalNumber n1 = new NaturalNumber(2);
-    NaturalNumber n2 = new NaturalNumber(1000);
-    PrimitiveRecursiveFunction suma = new Suma();
-    PrimitiveRecursiveFunction composition = new Composition(suma);
+    NaturalNumber n1 = new NaturalNumber(9);
+    NaturalNumber n2 = new NaturalNumber(30);
     PrimitiveRecursiveFunction combination = new Combination(1, 2, n1);
+    PrimitiveRecursiveFunction product = new Product();
+    PrimitiveRecursiveFunction composition = new Composition(product);
     System.out.println(composition.operate(combination.operate(n2)));
   }
 }
